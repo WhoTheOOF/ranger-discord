6ec26721e6a7f75e357eb754b3a81d1aec3aae83
CradReap <47833807+WhoTheOOF@users.noreply.github.com>
2020-03-29 00:38:05 -0500

    update memory ocmmand ok

--- cogs/Main.py
+++ cogs/Main.py
@@ -10,10 +10,10 @@ class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(aliases=['mem', 'memory'], hidden=True)
+    @commands.command(aliases=['mem', 'm'], hidden=True)
     @commands.is_owner()
-    async def m(self, ctx):
-        await ctx.send(f'You are currently using **{round(Process(getpid()).memory_info().rss/1024/1024, 2)} MB**')
+    async def memory(self, ctx):
+        await ctx.send(f'Ranger is currently using **{round(Process(getpid()).memory_info().rss/1024/1024, 2)} MB** of memory.')
 
     @commands.command()
     async def avatar(self, ctx, *, user: discord.User):
