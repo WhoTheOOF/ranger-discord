19af0e4af0b33db589766df0235a122c4684cd76
CradReap <47833807+WhoTheOOF@users.noreply.github.com>
2020-03-30 03:39:41 -0500

    Update Main.py

--- cogs/Main.py
+++ cogs/Main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import datetime
+import os
 import random
 import asyncio
 from psutil import Process
@@ -9,6 +10,47 @@ from os import getpid
 class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    @commands.command()
+    async def source(self, ctx, *, command: str = None):
+        """Displays my full source code or for a specific command."""
+        source_url = "https://github.com/WhoTheOOF/ranger-discord"
+        branch = "master"
+        if command is None:
+            return await ctx.send(f"support my dev by uh, giving a star on github; !\n{source_url}")
+
+        if command == "help":
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = self.bot.get_command(command.replace(".", " "))
+            if obj is None:
+                return await ctx.send("Could not find command.")
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if module.startswith("jishaku"):
+            location = module.replace(".", "/") + ".py"
+            source_url = "https://github.com/Gorialis/jishaku"
+            branch = "master"
+            final_url = f"<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>"
+            return await ctx.send(final_url)
+        if not module.startswith("discord"):
+            # not a built-in command
+            location = os.path.relpath(filename).replace("\\", "/")
+        else:
+            location = module.replace(".", "/") + ".py"
+            source_url = "https://github.com/Rapptz/discord.py"
+            branch = "master"
+
+        final_url = f"<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>"
+        await ctx.send(final_url)
         
     @commands.command(aliases=['mem', 'm'], hidden=True)
     @commands.is_owner()
