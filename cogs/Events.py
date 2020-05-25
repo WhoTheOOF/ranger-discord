718c996c50b024b91a672b3e421ad0a8d0b774d0
CradReap <47833807+WhoTheOOF@users.noreply.github.com>
2020-05-25 17:52:32 -0500

    add event for logs

--- cogs/Events.py
+++ cogs/Events.py
@@ -36,6 +36,26 @@ class Events(commands.Cog):
   async def on_guild_remove(self, guild):
     embed = discord.Embed(description="<a:blobhi:696885698766307421> **Left a server**\n\n**Name:** {}\n\n**ID:** {}\n\n**Owner:** {} (`{}`)".format(guild.name, guild.id, guild.owner.mention, guild.owner.id))
     await self.bot.get_channel(648010351186673664).send(embed=embed)
+    
+  @commands.Cog.listener()
+  async def on_member_join(self, member):
+
+    guild = member.guild
+    channel = self.bot.get_channel(702357773924696144)
+
+    if guild.id == 702355649237090424:
+
+      await channel.send(f"<a:blobhi:696885698766307421> {member.mention} ({member}) has joined **JumpKits**!")
+
+  @commands.Cog.listener()
+  async def on_member_remove(self, member):
+
+    guild = member.guild
+    channel = self.bot.get_channel(702357773924696144)
+
+    if guild.id == 702355649237090424:
+
+      await channel.send(f"<a:blobhi:696885698766307421> {member.mention} ({member}) has left **JumpKits**.")
       
 def setup(bot):
   bot.add_cog(Events(bot))
