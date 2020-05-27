5902af3035b05f0fe6fd1a7ff7c631eed895b924
CradReap <47833807+WhoTheOOF@users.noreply.github.com>
2020-05-26 23:07:06 -0500

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,16 +1,18 @@
-import ast
-import inspect
-import json
-import io
-import textwrap
-import traceback
-import aiohttp
-import random
-from contextlib import redirect_stdout
+import discord
 from discord.ext import commands
-from discord.voice_client import VoiceClient
-import discord, time, datetime, os, traceback
-from random import randint
+import time
+import aiohttp
+import io
+import json
+import textwrap
+import ast
+import datetime
+import os
+import traceback
+import typing
+import random
+import inspect
+import asyncio
 from jishaku import help_command
 t =  datetime.datetime.today()
 dt = t.strftime('%a, %d, %B, %Y, %I:%M %p')
@@ -103,6 +105,21 @@ async def r(ctx, cog: str):
     except Exception as e:
         print(f'{traceback.format_exc()}__________________________________________')
         await ctx.send(f'```py\n{e}\n```Full error in console')
+        
+def insert_returns(body):
+    # insert return stmt if the last expression is a expression statement
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    # for if statements, we insert returns into the body and the orelse
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
         
 @bot.command(aliases=['eval'], hidden=True)
 @commands.is_owner()
